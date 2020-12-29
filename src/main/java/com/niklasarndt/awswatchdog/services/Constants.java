c00e6dee933cf42cc132a1dfe5d6cc8564d86f7b
Niklas Arndt <git@niklasarndt.com>
2020-12-29 12:26:09 +0100

    Improve wording in mail text
    Signed-off-by: Niklas Arndt <git@niklasarndt.com>

--- src/main/java/com/niklasarndt/awswatchdog/services/Constants.java
+++ src/main/java/com/niklasarndt/awswatchdog/services/Constants.java
@@ -6,8 +6,8 @@ package com.niklasarndt.awswatchdog.services;
 public class Constants {
 
     public static final String MAIL_TEXT = "Hey there!<br><br>" +
-            "This is an automated message sent by the AWS S3 Watchdog. This message has been sent " +
-            "to inform you that the S3 Bucket <b>%s</b> received <i>%d</i> new objects.<br><br>" +
+            "This is an automated from AWS S3 Watchdog. This message has been sent " +
+            "to inform you that the S3 Bucket <b>%s</b> received <i>%d</i> new object(s).<br><br>" +
             "Here's a quick summary:<br><ul>%s</ul>" +
             "Goodbye \uD83D\uDE80<br>AWS S3 Watchdog v.%s (Build date: %s)";
 }
